2a45f87584f48fa06772e0ed259e2d35cd34393d
Jim Jiang <jim.jiang@optus.com.au>
2020-06-28 10:57:29 +1000

    added storybook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import Button from "./components/Button";
 import './App.css';
 
 function App() {
@@ -10,14 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <Button>Hello, Storybook!</Button>
+        </p>
       </header>
     </div>
   );
