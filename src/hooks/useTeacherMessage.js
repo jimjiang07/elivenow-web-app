c565d5983d5f4376c0761b95f67ad3c7c13e7adb
Jim Jiang <jim.jiang@optus.com.au>
2020-08-31 21:18:58 +1000

    fixed build

--- src/hooks/useTeacherMessage.js
+++ src/hooks/useTeacherMessage.js
@@ -1,7 +1,7 @@
 // Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { useContext, useEffect, useState, useCallback } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import { useHistory } from 'react-router-dom';
 import getChimeContext from '../context/getChimeContext';
